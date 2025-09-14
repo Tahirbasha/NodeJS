ad2e6c0033bc9dd9af670581b1fd0285947e304c
Tahirbasha <tahirbasha2626@gmail.com>
2025-09-14 10:18:13 +0530

    Http Methods and Nodemon Script

--- src/app.js
+++ src/app.js
@@ -6,8 +6,24 @@ app.use('/express', (req, res) => {
     res.send('Welcome to express js');
 });
 
-app.use((req, res) => {
+app.use('/welcome', (req, res) => {
     res.send('Welcome to node js');
 });
+
+app.use('/users', (req, res) => {
+    res.send('Generic');
+});
+
+app.get('/users', (req, res) => {
+    res.send('Get users');
+});
+
+app.post('/users', (req, res) => {
+    res.send('Post users');
+});
+
+app.delete('/users', (req, res) => {
+    res.send('Delete users')
+})
 
 app.listen(8080);
