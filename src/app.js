4e50162c6f8c9b363cddc5edee042975ab53c18d
Tahirbasha <tahirbasha2626@gmail.com>
2025-09-15 22:51:44 +0530

    Add - added getUsers and getUserByName Apis.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const User = require('./models/models');
 
 const app = express();
 
+app.use(express.json());
+
 connectDB().then(() => {
     console.log('Database is connected successfully')
     app.listen(8080);
@@ -12,23 +14,29 @@ connectDB().then(() => {
     console.log('Database not connected')
 })
 
-app.post('/signup', async(req, res) => {
-    const user = new User({
-        firstName: "John",
-        lastName: "Doe",
-        city: "Hyderabad",
-    });
+app.post('/signup', async (req, res) => {
+    const user = new User(req.body);
     await user.save();
     res.send("User created successfully");
-})
+});
 
-// app.use('/', (req, res, next) => {
-//     throw new Error('Something')
-//     res.send('User data');
-// });
-// app.use('/', (err, req, res, next) => {
-//     console.log(err)
-//     if (err) {
-//         res.status(500).send('Something went wrong');
-//     }
-// });
+app.get('/getUsers', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+app.get('/getUserByName', async (req, res) => {
+    try {
+        const name = req.body.name;
+        const users = await User.find({ firstName: name });
+        if (!users.length) {
+            res.status(400).send("User not found");
+        }
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+})
